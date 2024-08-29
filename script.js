bd51e5f394e6673777ac8aacc435dd5a0793899e
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-29 12:35:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-//your JS code here. If required.
-let promise = new Promise(function(resolve, reject) {
-	let promise1 = new Promise(function(resolve, reject) {
-  let time = Math.random() * (3000 - 1000) + 1000; // This will give a random time between 1 and 3 seconds
+// let promise = new Promise(function(resolve, reject) {
+// 	let promise1 = new Promise(function(resolve, reject) {
+//   let time = Math.random() * (3000 - 1000) + 1000;
+//   setTimeout(function(){
+//     resolve(time);
+//   }, time);
+// });
+// });
+let promise1 = new Promise(function(resolve, reject) {
+  let time = Math.random() * (3000 - 1000) + 1000;
   setTimeout(function(){
     resolve(time);
   }, time);
 });
+
+let promise2 = new Promise(function(resolve, reject) {
+  let time = Math.random() * (3000 - 1000) + 1000;
+  setTimeout(function(){
+    resolve(time);
+  }, time);
+});
+
+let promise3 = new Promise(function(resolve, reject) {
+  let time = Math.random() * (3000 - 1000) + 1000;
+  setTimeout(function(){
+    resolve(time);
+  }, time);
 });
