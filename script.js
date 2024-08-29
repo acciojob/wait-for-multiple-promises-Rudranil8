2f9a1440e0b9d62f404e673366254d003a2ea0db
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-08-29 12:31:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let promise = new Promise(function(resolve, reject) {
+	let promise1 = new Promise(function(resolve, reject) {
+  let time = Math.random() * (3000 - 1000) + 1000; // This will give a random time between 1 and 3 seconds
+  setTimeout(function(){
+    resolve(time);
+  }, time);
+});
+});
